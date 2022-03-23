5bdaaa9f18ca63e867d4a1e25b777606887fe57f
Funke <roiegol9@gmail.com>
2022-03-23 21:14:00 +0200

    Add a Place  Class

---
+++ models/Place.js
@@ -0,0 +1,9 @@
+class Place {
+    constructor(dimenssions, openPlace, popularity) {
+        this.dimenssions = dimenssions;
+        this.openPlace = openPlace;
+        this.popularity = popularity;
+    }
+}
+
+module.exports = Place;
