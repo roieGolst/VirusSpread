83ed3229bb7e1db25333ced09759dfeccaaed276
Funke <roiegol9@gmail.com>
2022-03-27 20:23:18 +0300

    Add npm to the project.

--- index.js
+++ index.js
@@ -5,12 +5,21 @@ const Virus = require("./models/Virus");
 let lsa = new Area(19, 1, 6);
 
 let roie = new Person(20, 1, []);
+let yoni = new Person(30, 0.8, []);
+let yakir =  new Person(5, 0.4, []);
 
 
-for(let i = 0; i < 10; i++){
-    lsa.placeAperson(
-        roie, 
-        Math.floor(Math.random() * lsa.dimenssions), 
-        Math.floor(Math.random() * lsa.dimenssions)
-    );
-}
+let map = new Map();
+
+map.set("roie", roie);
+map.set("yoni", yoni);
+map.set("yakir", yakir);
+
+map.delete("yoni")
+// for(let i = 0; i < 10; i++){
+//     lsa.placeAperson(
+//         roie, 
+//         Math.floor(Math.random() * lsa.dimenssions), 
+//         Math.floor(Math.random() * lsa.dimenssions)
+//     );
+// }
